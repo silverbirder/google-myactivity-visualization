06b633de2c7bef0666222f17d7fcde400fecd785
shibamoto <silverbirder@gmail.com>
2025-12-25 22:16:39 +0900

    fix: wrap footer links in a Flex container for improved layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,16 +5,29 @@ export const Footer = () => (
     <Text fontSize="sm">
       &copy; {new Date().getFullYear()} silverbirder. All rights reserved.
     </Text>
-    <Link
-      href="https://sites.google.com/view/silverbirders-services"
-      target="_blank"
-      rel="noopener noreferrer"
-      fontSize="sm"
-      color="blue.600"
-      textDecoration="underline"
-      _hover={{ color: "blue.800" }}
-    >
-      My Other Services
-    </Link>
+    <Flex gap="4">
+      <Link
+        href="https://sites.google.com/view/silverbirders-services"
+        target="_blank"
+        rel="noopener noreferrer"
+        fontSize="sm"
+        color="blue.600"
+        textDecoration="underline"
+        _hover={{ color: "blue.800" }}
+      >
+        My Other Services
+      </Link>
+      <Link
+        href="https://fequest.vercel.app/6"
+        target="_blank"
+        rel="noopener noreferrer"
+        fontSize="sm"
+        color="blue.600"
+        textDecoration="underline"
+        _hover={{ color: "blue.800" }}
+      >
+        Feature Requests
+      </Link>
+    </Flex>
   </Flex>
 );
